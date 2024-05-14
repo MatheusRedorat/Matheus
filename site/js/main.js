e0d97f830ac8ae5b30c2f6ead21d0b076917b708
Matheus Redorat <164897753+MatheusRedorat@users.noreply.github.com>
2024-05-14 09:54:26 -0300

    Delete site directory

--- site/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.slide-principal').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
